2745da91cb54a67eb77c822a299dbb017952ec35
Sterling Greene <sterling@gradle.com>
2020-05-07 02:19:53 -0400

    Polish the credentials via stdin sample

--- subprojects/docs/src/samples/tasks/pass-credentials-to-external-tool-via-stdin/common/login.sh
+++ subprojects/docs/src/samples/tasks/pass-credentials-to-external-tool-via-stdin/common/login.sh
@@ -3,14 +3,7 @@
 echo Enter username:
 read username
 echo Enter password:
-if [ -t 0 ] ; then # if connected to a terminal, do not echo the password input
-    stty -echo
-    read password
-    stty echo
-    echo
-else
-    read password
-fi
+read -s password
 
 if [ "$username" = "secret-user" ] && [ "$password" = "secret-password" ] ; then
     echo "Welcome, $username!"
--- subprojects/docs/src/samples/tasks/pass-credentials-to-external-tool-via-stdin/kotlin/build.gradle.kts
+++ subprojects/docs/src/samples/tasks/pass-credentials-to-external-tool-via-stdin/kotlin/build.gradle.kts
@@ -1,24 +1,21 @@
 val login = tasks.register<Exec>("login") {
-    val USERNAME_PROPERTY = "username"
-    val PASSWORD_PROPERTY = "password"
     val providers = project.getProviders()
-    val username = providers.gradleProperty(USERNAME_PROPERTY)
-    val password = providers.gradleProperty(PASSWORD_PROPERTY)
+    val username = providers.gradleProperty("username")
+    val password = providers.gradleProperty("password")
 
+    inputs.property("username", username)
+    inputs.property("password", password)
+    
     doFirst {
-        if (!username.isPresent() || !password.isPresent()) {
-            throw GradleException(String.format("login task requires '%s' and '%s' properties",
-                USERNAME_PROPERTY, PASSWORD_PROPERTY))
-        }
+        standardInput = java.io.ByteArrayInputStream("${username.get()}\n${password.get()}".toByteArray())
     }
 
-    standardInput = java.io.ByteArrayInputStream("${username.orNull}\n${password.orNull}".toByteArray())
     commandLine = listOf("sh", "login.sh")
 }
 
 tasks.register("doAuthenticated") {
     dependsOn(login)
     doLast {
-        println("Doing authenticated task")
+        logger.lifecycle("Doing authenticated task")
     }
 }
