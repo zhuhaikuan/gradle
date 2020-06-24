fab4be44909aa8c50443347ab93877df39163c57
Paul Merlin <paul@gradle.com>
2020-06-24 12:21:21 +0200

    Fix KotlinCompiler for breaking change in compiler internals
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/support/KotlinCompiler.kt
+++ subprojects/kotlin-dsl/src/main/kotlin/org/gradle/kotlin/dsl/support/KotlinCompiler.kt
@@ -25,7 +25,7 @@ import org.jetbrains.kotlin.cli.common.KOTLIN_COMPILER_ENVIRONMENT_KEEPALIVE_PRO
 import org.jetbrains.kotlin.cli.common.config.addKotlinSourceRoot
 import org.jetbrains.kotlin.cli.common.config.addKotlinSourceRoots
 
-import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSourceLocation
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.common.messages.MessageUtil
@@ -397,7 +397,7 @@ fun messageCollectorFor(log: Logger, pathTranslation: (String) -> String = { it 
 
 
 internal
-data class ScriptCompilationError(val message: String, val location: CompilerMessageLocation?)
+data class ScriptCompilationError(val message: String, val location: CompilerMessageSourceLocation?)
 
 
 internal
@@ -428,7 +428,7 @@ data class ScriptCompilationException(val errors: List<ScriptCompilationError>) 
         } ?: error.message
 
     private
-    fun errorAt(location: CompilerMessageLocation, message: String): String {
+    fun errorAt(location: CompilerMessageSourceLocation, message: String): String {
         val columnIndent = " ".repeat(5 + maxLineNumberStringLength + 1 + location.column)
         return "Line ${lineNumber(location)}: ${location.lineContent}\n" +
             "^ $message".lines().joinToString(
@@ -437,7 +437,7 @@ data class ScriptCompilationException(val errors: List<ScriptCompilationError>) 
     }
 
     private
-    fun lineNumber(location: CompilerMessageLocation) =
+    fun lineNumber(location: CompilerMessageSourceLocation) =
         location.line.toString().padStart(maxLineNumberStringLength, '0')
 
     private
@@ -449,7 +449,7 @@ data class ScriptCompilationException(val errors: List<ScriptCompilationError>) 
 
     private
     val maxLineNumberStringLength: Int by lazy {
-        errors.mapNotNull { it.location?.line }.max().toString().length
+        errors.mapNotNull { it.location?.line }.maxOrNull()?.toString()?.length ?: 1
     }
 }
 
@@ -470,7 +470,7 @@ class LoggingMessageCollector(
 
     override fun clear() = errors.clear()
 
-    override fun report(severity: CompilerMessageSeverity, message: String, location: CompilerMessageLocation?) {
+    override fun report(severity: CompilerMessageSeverity, message: String, location: CompilerMessageSourceLocation?) {
 
         fun msg() =
             location?.run {
